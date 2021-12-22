98fe71e641f1cc78269b0efaa23d323f2a2f1b31
Jeff El'Ducati <geomondi09@gmail.com>
2021-12-22 04:03:22 +0000

    added alchemy api link, deploy.js file

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/eIPnrtumDG7uRMh9IB9wV8HaO46Ea09q',
+      accounts: ['8f5b4a926c780fe0bb6c0554d55a99963451f6f104d711df8dfbed508599c115'],
+    },
+  },
 };
